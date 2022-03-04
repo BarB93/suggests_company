9b2f98da5a4cabfdfdf568b13cdacc82fdc91471
bbarovsky <barovskiyboris@gmail.com>
2022-03-04 03:19:30 +0300

    added debounce function for fetch during input type

--- src/components/SuggestCompany.js
+++ src/components/SuggestCompany.js
@@ -3,23 +3,24 @@ class SuggestCompany extends HTMLElement {
         this.template = suggestCompanyTmpl.content.cloneNode(true)
         this.attachShadow({mode: 'open'}).append(this.template)
         this.shadowRoot.querySelector('#party').addEventListener('input', this.inputSuggest)
+        this.fetchSuggestsDebounce = debounce(this.fetchSuggests, 500)
     }
 
     inputSuggest = (e) => {
-        this.fetchSuggests(e.currentTarget.value)
+        this.fetchSuggestsDebounce(e.currentTarget.value)
     }
 
     fetchSuggests = async (query) => {
         const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party'
         const token = '5a4dd6f0db4a97283eff4aa376df0c657b879b46'
-
+    
         var options = {
-            method: "POST",
-            mode: "cors",
+            method: 'POST',
+            mode: 'cors',
             headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": "Token " + token
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Token ' + token,
             },
             body: JSON.stringify({query: query})
         }
@@ -35,4 +36,14 @@ class SuggestCompany extends HTMLElement {
     }
 }
 
-customElements.define("time-formatted", SuggestCompany);
+customElements.define('time-formatted', SuggestCompany);
+
+// modules in real world =)
+function debounce (fn, ms) {
+    let timeout;
+    return function () {
+      const fnCall = () => { fn.apply(this, arguments) }
+      clearTimeout(timeout);
+      timeout = setTimeout(fnCall, ms)
+    };
+}
